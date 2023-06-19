869b94bd56cc966bf777eae14e423343b6fd9a9f
anurag-ay <ay1909069@gmail.com>
2023-06-19 23:15:43 +0530

    update check_env_vars.js

--- utils/check_env_vars.js
+++ utils/check_env_vars.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 module.exports = () => {
-  const { DATABASE_URI } = process.env;
+  const { DATABASE_URI, jwt_secret_key } = process.env;
 
-  if (!DATABASE_URI) {
+  if (!DATABASE_URI && !jwt_secret_key) {
     console.log("Don't Have environment varibales setup contact the Author");
     process.exit(1);
   }
