e0f9287e99636528a367bf1d8c12aa1547672d22
anurag-ay <ay1909069@gmail.com>
2023-06-22 23:39:18 +0530

    add custom validation messages

--- models/message.js
+++ models/message.js
@@ -3,13 +3,13 @@ const { userSchema } = require("./user");
 const Joi = require("joi");
 
 const messageSchema = new mongoose.Schema({
-  first_name: {
+  firstName: {
     type: String,
     minlength: 2,
     maxlength: 255,
     required: true,
   },
-  last_name: {
+  lastName: {
     type: String,
     minlength: 2,
     maxlength: 255,
@@ -18,7 +18,6 @@ const messageSchema = new mongoose.Schema({
   phone: {
     type: String,
     minlength: 10,
-    maxlength: 14,
   },
   email: {
     type: String,
@@ -37,11 +36,42 @@ const Message = mongoose.model("Messages", messageSchema);
 
 function validateMessage(contactInfo) {
   const schema = Joi.object({
-    first_name: Joi.string().min(2).max(255).required(),
-    last_name: Joi.string().min(2).max(255).required(),
-    email: Joi.string().email().min(2).max(255),
-    phone: Joi.string().required().min(10).max(14),
-    message: Joi.string().min(5).max(5000),
+    firstName: Joi.string().min(2).max(255).required().messages({
+      "string.base": "First name must be a string",
+      "string.empty": "First name is required",
+      "string.min": "First name must have at least {#limit} characters",
+      "string.max": "First name must have at most {#limit} characters",
+      "any.required": "First name is required",
+    }),
+
+    lastName: Joi.string().min(2).max(255).required().messages({
+      "string.base": "Last name must be a string",
+      "string.empty": "Last name is required",
+      "string.min": "Last name must have at least {#limit} characters",
+      "string.max": "Last name must have at most {#limit} characters",
+      "any.required": "Last name is required",
+    }),
+
+    email: Joi.string().email().min(2).max(255).messages({
+      "string.base": "Email must be a string",
+      "string.email": "Invalid email format",
+      "string.min": "Email must have at least {#limit} characters",
+      "string.max": "Email must have at most {#limit} characters",
+    }),
+
+    phone: Joi.string().required().min(10).messages({
+      "string.base": "Phone number must be a string",
+      "string.empty": "Phone number is required",
+      "string.min": "Phone number must have at least {#limit} digits",
+      "any.required": "Phone number is required",
+    }),
+
+    message: Joi.string().min(5).max(5000).messages({
+      "string.base": "Message must be a string",
+      "string.empty": "Message Should Not Be Empty",
+      "string.min": "Message must have at least {#limit} characters",
+      "string.max": "Message must have at most {#limit} characters",
+    }),
   });
   return schema.validate(contactInfo);
 }
